1d9d8debf02241bacef1715c411239073d383015
Anthony24601 <60018434+Anthony24601@users.noreply.github.com>
2024-02-26 15:51:17 -0700

    utility frame added

---
+++ utility.py
@@ -0,0 +1,23 @@
+
+nu = 1.12
+
+def calc_normalized_return(history):
+
+    pass
+
+def calc_weighted_return(history, k = 1):
+
+    pass
+
+def calc_L():
+    """
+    return-volatility correlation function
+    """
+    pass
+
+
+
+
+
+
+
